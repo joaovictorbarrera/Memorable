62cca48886126e6f25e37f7035af10f9598b4cd1
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-02-09 19:46:17 -0500

    Added ability to lazy load posts

--- Client/src/app/pages/home/home.ts
+++ Client/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PostCreate } from './components/post-create/post-create';
 import { PostDto } from '../../shared/models/post.dto';
 import { PostService } from '../../shared/services/post.service';
@@ -13,24 +13,59 @@ import { PostStore } from '../../shared/stores/post.store';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit {
+export class Home implements OnInit, AfterViewInit {
+  @ViewChild('scrollAnchor') scrollAnchor!: ElementRef;
+
+  private page = 1;
+  private readonly pageSize = 5;
+  public loading = false;
+  private hasMore = true;
+  private observer!: IntersectionObserver;
+
   constructor(
     private postService: PostService,
     public postStore: PostStore
   ) {}
 
-  ngOnInit(): void {
-    this.refreshFeed();
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          this.loadNextPage();
+        }
+      },
+      {
+        root: null,
+        rootMargin: '200px',
+        threshold: 0,
+      }
+    );
+
+    this.observer.observe(this.scrollAnchor.nativeElement);
   }
 
-  refreshFeed(): void {
-    // TODO: Lazy Load
-    this.postService.getFeed(10, 1).subscribe({
+  ngOnInit(): void {
+    this.loadNextPage();
+  }
+
+  loadNextPage(): void {
+    if (this.loading || !this.hasMore) return;
+
+    this.loading = true;
+
+    this.postService.getFeed(this.pageSize, this.page).subscribe({
       next: (posts: PostDto[]) => {
-        this.postStore.setAll(posts)
+        if (posts.length < this.pageSize) {
+          this.hasMore = false;
+        }
+
+        this.postStore.addManyPosts(posts);
+        this.page++;
+        this.loading = false;
       },
       error: (err) => {
-        window.alert("Error fetching feed: "+ err.message)
+        this.loading = false;
+        window.alert('Error fetching feed: ' + err.message);
       },
     });
   }
