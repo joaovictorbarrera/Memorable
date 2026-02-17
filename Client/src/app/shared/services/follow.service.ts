2467f42b970e1fbef8c2cbf768cbe23b56bccd3a
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-02-16 22:48:40 -0500

    Implemented Follow FE Service

---
+++ Client/src/app/shared/services/follow.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowService {
+  private readonly apiUrl = `${environment.apiUrl}/follow`;
+  constructor(private http: HttpClient) {}
+
+  /**
+   * FOLLOW
+   */
+  follow(userId: string): Observable<any> {
+    const params = new HttpParams()
+        .set('userId', userId);
+
+    return this.http.post(
+      `${this.apiUrl}/FollowCreate`,
+      { params }
+    );
+  }
+
+  /**
+   * UNFOLLOW
+   */
+  unfollow(userId: string): Observable<any> {
+    const params = new HttpParams()
+      .set('userId', userId);
+
+    return this.http.delete(
+      `${this.apiUrl}/FollowDelete`,
+      { params }
+    );
+  }
+}
