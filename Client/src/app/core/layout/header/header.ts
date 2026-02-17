dc252fc576449b9cf1e7ef6dbf7beb99d989e853
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-02-17 00:33:21 -0500

    Added active route highlighting

--- Client/src/app/core/layout/header/header.ts
+++ Client/src/app/core/layout/header/header.ts
@@ -1,16 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { ProfileIcon } from '../../../shared/components/profile-icon/profile-icon';
 import { GlobalService } from '../../state/global';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, MatIconModule, ProfileIcon],
+  imports: [RouterModule, MatIconModule, ProfileIcon, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-  constructor(public globalService: GlobalService) {
+  constructor(public globalService: GlobalService, private router: Router) {
+  }
+
+  isActive(route: string): boolean {
+    console.log(this.router.url, route)
+    return this.router.url === route;
   }
 }
